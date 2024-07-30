a5225efd8a1ee07e33ecca94962227980b58f3a1
saengle <100755761+saengle@users.noreply.github.com>
2024-07-31 00:13:04 +0900

    Initial Commit

---
+++ RxSwiftPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftPractice
+//
+//  Created by 쌩 on 7/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
