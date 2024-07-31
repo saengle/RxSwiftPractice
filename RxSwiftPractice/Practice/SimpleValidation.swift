371c0f58671ff9c5eb3cc6f2b52d51b20586afdc
saengle <100755761+saengle@users.noreply.github.com>
2024-07-31 21:40:30 +0900

    Simple Validation

--- RxSwiftPractice/Practice/SimpleValidation.swift
+++ RxSwiftPractice/Practice/SimpleValidation.swift
@@ -10,37 +10,110 @@ import RxCocoa
 import RxSwift
 
 class SimpleValidation: UIViewController {
-    let number1 = UITextField()
-    let number2 = UITextField()
-    let number3 = UITextField()
+    let nameTF = UITextField()
+    let passwordTF = UITextField()
+
+    let nameValidationLabel = UILabel()
+    let passwordValidationLabel = UILabel()
     
-    let result = UILabel()
+    let button = UIButton()
     
     let disposeBag = DisposeBag()
+    
+    let minimalLength = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        nameValidationLabel.text = "유저의 이름은 최소 \(minimalLength)글자 이상이여야합니다."
+        passwordValidationLabel.text = "유저의 이름은 최소 \(minimalLength)글자 이상이여야합니다."
+        
+        let usernameValid = nameTF.rx.text.orEmpty
+            .map{ $0.count >= self.minimalLength }
+            .share(replay: 1)
+        
+        let passwordValid = passwordTF.rx.text.orEmpty
+            .map{ $0.count >= self.minimalLength }
+            .share(replay: 1)
+        
+        let everythingValid = Observable.combineLatest(usernameValid, passwordValid) { $0 && $1}
+            .share(replay: 1)
+        
+        usernameValid
+            .bind(to: passwordTF.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        usernameValid
+            .bind(to: nameValidationLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        passwordValid
+            .bind(to: passwordValidationLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+
+        everythingValid
+            .bind(to: button.rx.isEnabled)
+            .disposed(by: disposeBag)
+
+        button.rx.tap
+            .subscribe(onNext: { [weak self] _ in self?.showAlert() })
+            .disposed(by: disposeBag)
     }
 }
 
 extension SimpleValidation {
+    func showAlert() {
+        let alert = UIAlertController(
+            title: "RxExample",
+            message: "This is wonderful",
+            preferredStyle: .alert
+        )
+        let defaultAction = UIAlertAction(title: "Ok",
+                                          style: .default,
+                                          handler: nil)
+        alert.addAction(defaultAction)
+        present(alert, animated: true, completion: nil)
+    }
     private func setUI() {
-        view.addSubview(number1)
-        view.addSubview(number2)
-        view.addSubview(number3)
-        view.addSubview(result)
-        number1.snp.makeConstraints { make in
-            
+        view.backgroundColor = .white
+        view.addSubview(nameTF)
+        view.addSubview(passwordTF)
+        view.addSubview(nameValidationLabel)
+        view.addSubview(passwordValidationLabel)
+        view.addSubview(button)
+        nameTF.backgroundColor = .lightGray
+        passwordTF.backgroundColor = .lightGray
+        nameValidationLabel.backgroundColor = .lightGray
+        passwordValidationLabel.backgroundColor = .lightGray
+        button.backgroundColor = .blue
+        nameTF.snp.makeConstraints { make in
+            make.width.equalTo(300)
+            make.height.equalTo(50)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(200)
         }
-        number2.snp.makeConstraints { make in
-            
+        nameValidationLabel.snp.makeConstraints { make in
+            make.width.equalTo(300)
+            make.height.equalTo(50)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(nameTF).inset(70)
         }
-        number3.snp.makeConstraints { make in
-            
+        passwordTF.snp.makeConstraints { make in
+            make.width.equalTo(300)
+            make.height.equalTo(50)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(nameValidationLabel).inset(100)
         }
-        result .snp.makeConstraints { make in
-            
+        passwordValidationLabel .snp.makeConstraints { make in
+            make.width.equalTo(300)
+            make.height.equalTo(50)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(passwordTF).inset(70)
+        }
+        button.snp.makeConstraints { make in
+            make.width.height.equalTo(50)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(passwordValidationLabel).inset(100)
         }
     }
 }
